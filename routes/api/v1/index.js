037d18185c12e63f225e7541c957736d725f3301
Andy Lo <andy@odenterprise.org>
2018-01-24 09:36:19 -0600

    Added API routes.

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -8,7 +8,7 @@ const models = require('../../../models');
 
 router.get('/', (req, res) => {
   try {
-    res.send(req.query);
+    res.send();
   } catch (err) {
     console.log(err);
   }
@@ -19,18 +19,108 @@ router.get('/resources/', [
     check('offset').optional().isInt({ min: 0 }).withMessage('must be a positive integer'),
     check('country').optional().isAlpha().withMessage('must contain only letters'),
   ], async (req, res, next) => {
+    // Validate paramaters.
     const errors = validationResult(req);
-
     if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.mapped() });
+      res.status(422).json({ errors: errors.mapped() }).send();
     }
 
     try {
-      models.resource.findAll().then((values) => {
-        res.send(values);
+      // Set parameter defaults here.
+      const limit = req.query.limit ? req.params.limit : 100;
+      const offset = req.query.offset ? req.params.offset : 0;
+
+      // List detailing filter specifications.
+      let filterList = {
+        'type': {
+          model: 'resource_type',
+          filteringField: 'resource_id',
+          retrieveFields: [],
+        },
+        'country': {
+          model: 'country',
+          filteringField: 'country_id',
+          retrieveFields: [],
+        },
+        'topic': {
+          association: 'topics',
+          model: 'topic',
+          filteringField: 'tag',
+          retrieveFields: ['id'],
+        },
+        'language': {
+          association: 'languages',
+          model: 'language',
+          filteringField: 'ietf_tag',
+          retrieveFields: ['ietf_tag'],
+        },
+      };
+
+      // Set filters.
+      let filters = {
+        publish: true, // Default filter - only published resources.
+      };
+      let assocFilters = {};
+
+      for (let filterName in filterList) {
+        if (filterName in req.query) {
+          let filter = filterList[filterName];
+
+          if (filter.filteringField in models.resource.attributes) {
+            // Add filter to direct filters.
+            filters[filterName] = req.query[filterName];
+          } else if ('association' in filter &&
+            filter.association in models.resource.associations) {
+            // Add filters to many-to-many association filters.
+            assocFilters[filterName] = req.query[filterName];
+          }
+          // Ignore unmatched filters.
+        }
+      }
+
+      // Set filters for many-to-many relations.
+      let associations = [];
+
+      for (let filterName in assocFilters) {
+        let filter = filterList[filterName];
+        let filterValues = assocFilters[filterName];
+
+        // Only use the filter if there are filtering values provided.
+        if (filterValues.length) {
+          let assocation = {};
+          assocation.model = models[filter.model];
+          assocation.attributes = filter.retrieveFields;
+          assocation.through = {
+            attributes: [],
+          };
+          assocation.where = {};
+          assocation.where[filter.filteringField] = filterValues.split(',');
+
+          associations.push(assocation);
+        }
+      }
+
+      models.resource.findAll({
+        attributes: [
+          'title',
+          'organization',
+          'url',
+          'date_published',
+          'image_url',
+          'description',
+          'publish',
+        ],
+        include: associations,
+        limit: limit,
+        offset: offset,
+        where: filters,
+        raw: false,
+      }).then((results) => {
+        res.send(results);
       });
     } catch (err) {
       console.log(err);
+      res.status(500).send();
     }
   }
 );
@@ -55,6 +145,43 @@ router.get('/resources/:uuid', [
     }
   }
 );
+
+router.get('/resource_types/', async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.mapped() });
+    }
+
+    try {
+      models.resource_type.findAll().then((values) => {
+        res.send(values);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+
+router.get('/resource_types/:id', async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.mapped() });
+    }
+
+    try {
+      let id = req.params.id;
+
+      models.resource_type.findById(id).then((value) => {
+        res.send(value);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+
 
 router.get('/topics/', [
     check('limit').optional().isInt({ min: 0 }).withMessage('must be a positive integer'),
@@ -87,6 +214,45 @@ router.get('/topics/:id', async (req, res, next) => {
       let id = req.params.id;
 
       models.topic.findById(id).then((value) => {
+        res.send(value);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+
+router.get('/languages/', [
+    check('limit').optional().isInt({ min: 0 }).withMessage('must be a positive integer'),
+    check('offset').optional().isInt({ min: 0 }).withMessage('must be a positive integer'),
+  ], async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.mapped() });
+    }
+
+    try {
+      models.language.findAll().then((values) => {
+        res.send(values);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+
+router.get('/languages/:id', async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.mapped() });
+    }
+
+    try {
+      let id = req.params.id;
+
+      models.language.findById(id).then((value) => {
         res.send(value);
       });
     } catch (err) {
