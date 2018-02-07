2793063eb21ed3e67d3af7346d80af34bede05f6
Andy Lo <alo@sunlightfoundation.com>
2018-02-07 14:48:58 -0600

    Implemented resouce image upload to S3.

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var app = express();
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
---
+++ routes/forest/upload.js
@@ -0,0 +1,59 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const liana = require('forest-express-sequelize');
+const AWS = require('aws-sdk');
+
+function detectBase64MimeType (data) {
+  let mimeType = null;
+
+  if (typeof data === 'string') {
+    mimeType = data.split(';')[0].split(':')[1];
+  }
+
+  return mimeType;
+}
+
+function uploadImage (req, res, next) {
+  const AWSConfig = {
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    },
+    region: process.env.AWS_REGION,
+    params: { Bucket: process.env.S3_BUCKET }
+  };
+
+  // Create the S3 client.
+  const s3Bucket = new AWS.S3(AWSConfig);
+
+  const resourceUUID = req.body.data.id;
+  const rawData = req.body.data.attributes.image_url;
+  // Parse the data "URL" scheme (RFC 2397).
+  const mimeType = detectBase64MimeType(rawData);
+  const base64Image = rawData.replace(/^data:image\/\w+;base64,/, '');
+
+  var data = {
+    Key: resourceUUID,
+    Body: Buffer.from(base64Image, 'base64'),
+    ContentEncoding: 'base64',
+    ContentType: mimeType,
+    ACL: 'public-read'
+  };
+
+  // Upload the image.
+  s3Bucket.upload(data, function (error, data) {
+     if (error) { return next(error); }
+
+     // Inject the new image URL to the params.
+     req.body.data.attributes.image_url = data.Location;
+     console.log(data.Location);
+
+     // Finally, call the default PUT behavior.
+     next();
+  });
+}
+
+router.put('/resource/:resourceId', liana.ensureAuthenticated, uploadImage);
+
+module.exports = router;
