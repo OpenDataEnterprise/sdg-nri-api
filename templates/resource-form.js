a13b623220833486ca1356a1dd9e4ddcaa2208d9
Andy Lo <andy@odenterprise.org>
2018-02-21 11:36:22 -0600

    Updated e-mail templates and form handling.

--- templates/resource-form.js
+++ templates/resource-form.js
@@ -11,41 +11,47 @@ const AWSConfig = {
   apiVersion: '2010-12-01',
 };
 
-const templateName = 'ContactFormTemplate';
+const templateName = 'ResourceFormTemplate';
 
 const htmlTemplate = [
+  'A new resource submission has been received. Please log into ',
+  '<a href="https://app.forestadmin.com/">Forest</a>',
+  ' for further details and action.<br>',
+  '<bold>Submission UUID:</bold> {{ submission_uuid }}<br>',
+  '<bold>Resource UUID:</bold> {{ resource_uuid }}<br>',
   '<bold>Name:</bold> {{ firstName }} {{ lastName }}<br>',
   '<bold>E-mail address:</bold> {{ emailAddress }}<br>',
   '<bold>Organization:</bold> {{ organization }}<br>',
   '<bold>Title:</bold> {{ title }}<br>',
   '<bold>Country:</bold> {{ country }}<br>',
   '<bold>City:</bold> {{ city }}<br>',
-  '<bold>Message:</bold> {{ message }}<br>',
-  '<bold>I am interested in:</bold> {{ interests }}',
 ];
 
 const textTemplate = [
+  'A new resource submission has been received. Please log into ',
+  '<a href="https://app.forestadmin.com/">Forest</a>',
+  ' for further details and action.\n',
+  'Submission UUID: {{ submission_uuid }}\n',
+  'Resource UUID: {{ resource_uuid }}',
   'Name: {{ firstName }} {{ lastName }}\n',
   'E-mail address: {{ emailAddress }}\n',
   'Organization: {{ organization }}\n',
   'Title: {{ title }}\n',
   'Country: {{ country }}\n',
   'City: {{ city }}\n',
-  'Message: {{ message }}\n',
-  'I am interested in: {{ interests }}',
 ];
 
-const contactFormTemplate = {
+const resourceFormTemplate = {
   'Template': {
     'TemplateName': templateName,
-    'SubjectPart': 'Form Submission - SDG National Reporting Initiative Contact Form',
+    'SubjectPart': 'Form Submission - SDG National Reporting Initiative Resource Submission Form',
     'HtmlPart': htmlTemplate.join(''),
     'TextPart': textTemplate.join(''),
   },
 };
 
-function createContactFormTemplate() {
-  var createPromise = new AWS.SES(AWSConfig).createTemplate(contactFormTemplate).promise();
+function createResourceFormTemplate() {
+  var createPromise = new AWS.SES(AWSConfig).createTemplate(resourceFormTemplate).promise();
 
   createPromise.then(function(data) {
     console.log(data.MessageId);
@@ -54,7 +60,7 @@ function createContactFormTemplate() {
   });
 }
 
-function deleteContactFormTemplate() {
+function deleteResourceFormTemplate() {
   var deletePromise = new AWS.SES(AWSConfig).deleteTemplate({
     TemplateName: templateName
   }).promise();
@@ -66,6 +72,6 @@ function deleteContactFormTemplate() {
   });
 }
 
-exports.contactFormTemplate = contactFormTemplate;
-exports.createContactFormTemplate = createContactFormTemplate;
-exports.deleteContactFormTemplate = deleteContactFormTemplate;
+exports.resourceFormTemplate = resourceFormTemplate;
+exports.createResourceFormTemplate = createResourceFormTemplate;
+exports.deleteResourceFormTemplate = deleteResourceFormTemplate;
