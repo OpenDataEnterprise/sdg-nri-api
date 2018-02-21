aab9f9f61dc4e01227128e0fa25d9f9b36385ea4
Andy Lo <andy@odenterprise.org>
2018-02-21 13:32:59 -0600

    Inserted missing comma.

--- models/submission_status.js
+++ models/submission_status.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
-    }
+    },
     status: {
       type: DataTypes.TEXT,
     },
