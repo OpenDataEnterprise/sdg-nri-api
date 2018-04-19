5b94212af174c1be2322469074828addf6dd1fb3
Andy Lo <alo@odenterprise.org>
2018-04-19 14:48:50 -0500

    Migration downgrade fixes and cleanup.

--- sql/migration/up/4-5.sql
+++ sql/migration/up/4-5.sql
@@ -1,12 +1,12 @@
 -- Create publication timestamp field.
 ALTER TABLE sdg.news
-    ADD COLUMN published_at TIMESTAMPTZ NOT NULL DEFAULT now();
+    ADD COLUMN IF NOT EXISTS published_at TIMESTAMPTZ NOT NULL DEFAULT now();
 
 -- Create publication flags.
 ALTER TABLE sdg.news
-    ADD COLUMN publish BOOLEAN NOT NULL DEFAULT FALSE;
+    ADD COLUMN IF NOT EXISTS publish BOOLEAN NOT NULL DEFAULT FALSE;
 ALTER TABLE sdg.event
-    ADD COLUMN publish BOOLEAN NOT NULL DEFAULT FALSE;
+    ADD COLUMN IF NOT EXISTS publish BOOLEAN NOT NULL DEFAULT FALSE;
 
 -- Set all existing news and events to be published by default.
 UPDATE sdg.news SET publish = true;
