baf945c5526a1dca7e6f58b265ee54eb137d5034
Andy Lo <andy@odenterprise.org>
2018-04-19 11:20:01 -0500

    Added migration management with pg-migrator.

---
+++ sql/migration/down/3-2.sql
@@ -0,0 +1,28 @@
+DROP TEXT SEARCH CONFIGURATION IF EXISTS sdg.english_nostop CASCADE;
+DROP TEXT SEARCH DICTIONARY IF EXISTS sdg.english_stem_nostop CASCADE;
+
+/* Disable update trigger from working while bulk updating. */
+SET sdg.resource_tsv_update_trigger_disabled = 'true';
+
+/* Revert all search indices to previous version. */
+UPDATE sdg.resource SET
+    tsv =
+        setweight(to_tsvector('english', COALESCE(title, '')), 'A') ||
+        setweight(to_tsvector('english', COALESCE(organization, '')), 'B') ||
+        setweight(to_tsvector('sdg.english_nostop', COALESCE(array_to_string(tags, ','), '{}')), 'C') ||
+        setweight(to_tsvector('sdg.english_nostop', COALESCE(description, '')), 'D'),
+    updated_at = CURRENT_TIMESTAMP;
+
+/* Reenable trigger. */
+SET sdg.resource_tsv_update_trigger_disabled = 'false';
+
+/* Rollback search index update function to previous version. */
+CREATE FUNCTION resource_tsv_update_trigger() RETURNS trigger AS $$
+BEGIN
+    NEW.tsv :=
+        setweight(to_tsvector('english', COALESCE(NEW.title,'')), 'A') ||
+        setweight(to_tsvector('english', COALESCE(NEW.organization,'')), 'B') ||
+        setweight(to_tsvector('english', COALESCE(NEW.description,'')), 'C');
+    RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
---
+++ sql/migration/down/5-4.sql
@@ -0,0 +1,5 @@
+ALTER TABLE sdg.news DROP COLUMN IF EXISTS publication_date;
+
+ALTER TABLE sdg.news DROP COLUMN IF EXISTS publish;
+
+ALTER TABLE sdg.event DROP COLUMN IF EXISTS publish;
